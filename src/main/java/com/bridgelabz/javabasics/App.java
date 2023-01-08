12a22ca95213446970a96cb165cb0144812f20d4
AryaBhuwan-98 <aryabhuwan873@gmail.com>
2023-01-07 22:09:42 -0800

    Add:Welcome to the JAVA BASIC PROGRAM

--- src/main/java/com/bridgelabz/javabasics/App.java
+++ src/main/java/com/bridgelabz/javabasics/App.java
@@ -1,13 +1,9 @@
 package com.bridgelabz.javabasics;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Welcome TO JAVA BASICS day1" );
     }
 }
