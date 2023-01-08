c03e592fad723c1e835daf0bef5506803e500016
AryaBhuwan-98 <aryabhuwan873@gmail.com>
2023-01-07 22:22:10 -0800

    Add:Adding program for compare two STRING are EQUAL

---
+++ src/main/java/com/bridgelabz/javabasics/StringCompare.java
@@ -0,0 +1,11 @@
+package com.bridgelabz.javabasics;
+
+public class StringCompare {
+    public static void main(String[] args) {
+        String s1="Bridgelabz";
+String s2="Bridgelabz";
+if (s1 == s2) {
+System.out.println(s1==s2);
+}
+    }
+}
