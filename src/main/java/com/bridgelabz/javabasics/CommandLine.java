e5208d3f7f9840e403bec683df1d9d6d70df06dc
AryaBhuwan-98 <aryabhuwan873@gmail.com>
2023-01-07 22:16:27 -0800

    Add:Adding program of command line

---
+++ src/main/java/com/bridgelabz/javabasics/CommandLine.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.javabasics;
+
+public class CommandLine {
+    public static void main(String[] args) {
+        String name = args[0];
+        System.out.println("The name of the user is: " +name);
+    }
+
+}
